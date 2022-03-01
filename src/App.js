6698f3c597a04e285226fcd80d9f11527db4108a
TaraMKelly <tara.kelly16@gmail.com>
2022-03-01 12:21:59 -0500

    hangman functionality works

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Header from './components/Header';
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['guitar', 'dependencies', 'notebook', 'turntable'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 
@@ -14,13 +14,40 @@ function App() {
   const [correctLetters, setCorrectLetters] = useState([])
   const [wrongLetters, setWrongLetters] = useState([])
 
+  useEffect(() => {
+    const handleKeyDown = e => {
+      const { key, keyCode } = e
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeyDown)
+    
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [correctLetters, wrongLetters, playable])
+
+
   return (
     <>
       <Header />
-      <div className = "game-container">
-        <Figure />
-        <WrongLetters wrongLetters={wrongLetters}/>
-        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters}/>
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
